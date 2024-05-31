e9ea5be8a1b0167dc312b7f25e323b57d9337684
Caleb Yoong <caleb.yoong216@gmail.com>
2024-05-31 10:07:30 +0800

    changed server service uuid

--- IOT-Project-BLE-Server.ino
+++ IOT-Project-BLE-Server.ino
@@ -11,7 +11,7 @@
 // See the following for generating UUIDs:
 // https://www.uuidgenerator.net/
 
-#define SERVICE_UUID        "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
+#define SERVICE_UUID        "4fafc201-1fb5-459e-8fcc-c5c9c331914c"
 #define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
 
 class MyCallbacks : public BLECharacteristicCallbacks {
